9f8b0d86ec9d58922d40758784e0b46c83480963
AMNA25 <amna.bibi@emumba.com>
2021-04-16 10:10:20 +0500

    Added function to read data from csv file

--- src/main/java/pages/ReadAndWriteExcelDataPage.java
+++ src/main/java/pages/ReadAndWriteExcelDataPage.java
@@ -2,16 +2,22 @@ package pages;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReadAndWriteExcelDataPage {
-    File file = new File("D:\\twic_Automation\\src\\main\\java\\pages\\ReadAndWriteExcelDataPage.java");
-    FileInputStream fileInputStream = new FileInputStream(file);
-    XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
-   // XSSFSheet sheet = new XSSFSheet("Login");
-    public ReadAndWriteExcelDataPage() throws IOException {
+    public String readData(int rowValue, int cellValue) throws IOException {
+        File file = new File("src/main/java/utils/Twic-data.xlsx");
+        FileInputStream fileInputStream = new FileInputStream(file);
+        XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+        XSSFSheet sheet = workbook.getSheet("Login");
+        XSSFRow row = sheet.getRow(rowValue);
+        XSSFCell cell = row.getCell(cellValue);
+        String value = cell.getStringCellValue();
+        return value;
     }
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,17 +1,22 @@
 import org.junit.Test;
 import pages.LoginPage;
+import pages.ReadAndWriteExcelDataPage;
+
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest extends BaseTest{
-    String email = "max@twic.ai";
-    String password = "foobar";
+    //String email = "max@twic.ai";
+    //String password = "foobar";
 
     @Test
-    public void loginSuccessful(){
+    public void loginSuccessful() throws IOException {
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         //System.out.println("This is driver "+driver);
         LoginPage loginpage = new LoginPage(driver);
-
+        ReadAndWriteExcelDataPage data = new ReadAndWriteExcelDataPage();
+        String email = data.readData(2, 0);
+        String password = data.readData(2, 1);
         loginpage.positiveLogin(email, password);
        // HomePage homepage = new HomePage();
        //Assert.assertEquals(loginpage.getMenuButtonText(), "  ");
